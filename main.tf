d29463492ff4ac78a491e9e500d258fd4eb81141
Michał Oręziak <micorix.dev@gmail.com>
2020-12-20 00:13:20 +0100

    Fixed domain module vars

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "cloudflare_email" {}
+variable "cloudflare_api_key" {}
+
 module "domains" {
   source = "./domains"
+  cloudflare_email = var.cloudflare_email
+  cloudflare_api_key = var.cloudflare_api_key
 }
