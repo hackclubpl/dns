1efdf5ba2ec8cfe3ba1155984f29a8c4ff2316f6
Michał Oręziak <micorix.dev@gmail.com>
2020-12-20 14:07:26 +0100

    [domains] Add vercel_ip_address & hackclub_main_domain variables

--- domains/hackclub.blog.tf
+++ domains/hackclub.blog.tf
@@ -2,6 +2,17 @@ resource "cloudflare_zone" "hackclubblog" {
   zone = "hackclub.blog"
 }
 
+# Cloudflare requires proxied record to enable page rules
+resource "cloudflare_record" "hackclubblog_a_main" {
+  zone_id = cloudflare_zone.hackclubblog.id
+
+  name  = "@"
+  type  = "A"
+  value = var.vercel_ip_address
+
+  proxied = true
+}
+
 resource "cloudflare_page_rule" "hackclubblog_redirect" {
   zone_id = cloudflare_zone.hackclubblog.id
   target  = "*${cloudflare_zone.hackclubblog.zone}/*"
@@ -9,7 +20,7 @@ resource "cloudflare_page_rule" "hackclubblog_redirect" {
   actions {
     forwarding_url {
       status_code = 302
-      url         = "https://hackclub.pl"
+      url         = "https://${var.hackclub_main_domain}"
     }
   }
 }
--- domains/hackclub.events.tf
+++ domains/hackclub.events.tf
@@ -2,6 +2,17 @@ resource "cloudflare_zone" "hackclubevents" {
   zone = "hackclub.events"
 }
 
+# Cloudflare requires proxied record to enable page rules
+resource "cloudflare_record" "hackclubevents_a_main" {
+  zone_id = cloudflare_zone.hackclubevents.id
+
+  name  = "@"
+  type  = "A"
+  value = var.vercel_ip_address
+
+  proxied = true
+}
+
 resource "cloudflare_page_rule" "hackclubevents_redirect" {
   zone_id = cloudflare_zone.hackclubevents.id
   target  = "*${cloudflare_zone.hackclubevents.zone}/*"
@@ -9,7 +20,7 @@ resource "cloudflare_page_rule" "hackclubevents_redirect" {
   actions {
     forwarding_url {
       status_code = 302
-      url         = "https://hackclub.pl"
+      url         = "https://${var.hackclub_main_domain}"
     }
   }
 }
